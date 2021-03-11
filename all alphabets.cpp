86869dccf901070ec70bb2dbf4a4e70e5467ff75
MuhammadTayyab26 <bcsm-f16-526@superior.edu.pk>
2021-03-11 23:10:11 +0500

    Print Alphabets using for loop in C++

---
+++ all alphabets.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	char c;
+	for( c = 'A' ;  c<= 'Z'; c++)
+	{
+		cout<<"the  alphabets is "<<c<<endl;
+	}
+getch();
+return 0;
+}
+
